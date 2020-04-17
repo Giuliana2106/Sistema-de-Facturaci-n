78d75a70c1fab8a8c5f1e6b1a039b7f5cfb2f678
Giuliana2106 <62736506+Giuliana2106@users.noreply.github.com>
2020-04-17 15:40:12 -0600

    Ya se trabajo la primera parte, si ves que no hay algo bueno, puedes cambiarlo

---
+++ Sistema/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string nit;
+            string op;
+            double sub1;
+            string pro1;
+            double pre1;
+            int cant1;
+            string linea;
+
+            Console.Write("Ingrese nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese nit: ");
+            nit = Console.ReadLine();
+
+            sub1 = 0;
+
+            while (Op != n) ;
+
+            true;
+            Console.Write("Ingrese nombre del producto: ");
+            pro1 = Console.ReadLine();
+
+            Console.Write("Ingrese precio del producto: ");
+            linea = Console.ReadLine();
+            pre1 = double.Parse(linea);
+
+            Console.Write("Ingrese cantidad del producto: ");
+            linea = Console.ReadLine();
+            cant1 = double.Parse();
+
+            Sub1 = Sub1 + (pre1 + cant1);
+
+            Console.Write("¿Desea agregar otro producto?: ");
+            n = string.Parse(linea);
+            s = string.Parse(linea);
+
+            false;
+                Console.Write("El total de la compra es: +sub1");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
